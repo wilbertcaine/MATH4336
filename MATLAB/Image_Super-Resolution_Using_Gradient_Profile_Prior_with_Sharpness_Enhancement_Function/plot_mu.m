e3e7350fe1a868de994dc359236ce744874eb505
Wilbert Caine <44130928+wcaine@users.noreply.github.com>
2021-04-03 16:17:41 +0800

    Add files via upload

---
+++ MATLAB/Image_Super-Resolution_Using_Gradient_Profile_Prior_with_Sharpness_Enhancement_Function/plot_mu.m
@@ -0,0 +1,73 @@
+function plot_mu(mu)
+    up_scale = 2;
+    addpath('\\Vdidrive\myhome\wcaine\Documents\MATLAB\input\Set14');
+
+    ground_truth = imread('ppt3.png');
+    up_scale_factor = 3;
+    t_max = 25;
+    tau = 0.2; % 0.2
+    beta = 0.05; % 0.05
+    lambda_h = 1.63;
+    lambda_l = 1.6;
+    gau_sigma = 0.8;
+
+    ground_truth = rgb2gray(ground_truth);
+    ground_truth = im2double(ground_truth);
+
+    input = imresize(ground_truth, 1/up_scale^up_scale_factor, 'bilinear');
+    up_input = imresize(input, up_scale^up_scale_factor, 'bicubic');
+    img = input;
+
+    for scale = 1 : up_scale_factor
+        img_input = img;
+        img = imresize(img, up_scale, 'bicubic');
+
+        for iter = 1 : t_max
+        disp(iter);
+            img_filter = imgaussfilt(img, gau_sigma);
+            img_filter_down = imresize(img_filter, 1/up_scale, 'bilinear');
+            rc = img_filter_down - img_input;
+            rc_up = imresize(rc, up_scale, 'bicubic');
+            rc_up_filter = imgaussfilt(rc_up, gau_sigma);
+
+            [Gmag,Gdir] = imgradient(img, 'central');
+            Gmag = imcomplement(Gmag);
+            [Gc,Gr] = imgradientxy(img, 'central');
+            [sigma_l_matrix, distance_l_matrix] = sigma_distance_matrix(Gmag, Gc, Gr);
+            sigma_h_matrix = find_sigma_h(sigma_l_matrix, mu);
+            [Gc_T, Gr_T] = find_transform_ratio(distance_l_matrix, Gr, Gc, sigma_h_matrix, sigma_l_matrix, lambda_h, lambda_l);
+            [Gc_Gr, Gr_Gr] = imgradientxy(Gr, 'central');
+            [Gc_Gc, Gr_Gc] = imgradientxy(Gc, 'central');
+            [Gc_Gr_T, Gr_Gr_T] = imgradientxy(Gr_T, 'central');
+            [Gc_Gc_T, Gr_Gc_T] = imgradientxy(Gc_T, 'central');
+
+            Div_G = Gr_Gr + Gc_Gc;
+            Div_G_T = Gr_Gr_T + Gc_Gc_T;
+            gc = Div_G - Div_G_T;
+
+            img = img - tau * (rc_up_filter - beta * gc);
+            img = min(img, 1);
+            img = max(img, 0);
+            if mod(iter, 5) == 0
+                pos = 0;
+                if mu > 0
+                    pos = up_scale_factor+3;
+                end
+                subplot(2, up_scale_factor+3, pos+1);
+                imshow(input);
+                title('input');
+                subplot(2, up_scale_factor+3, pos+2);
+                imshow(up_input);
+                title('bicubic');
+                subplot(2, up_scale_factor+3, pos+scale+2);
+                imshow(img);
+                title([num2str(up_scale^scale), 'X']);
+                subplot(2, up_scale_factor+3, pos+up_scale_factor+3);
+                imshow(ground_truth);
+                title('ground truth');
+                pause(0.01);
+            end
+        end
+    end
+end
+
